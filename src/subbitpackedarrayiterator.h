81d3fa1e37b7f6558f4ea5ef47f2d2b6eb76669c
Christian Amann <christian.amann@k-type.de>
2023-08-14 13:19:40 +0200

    Do chunk unpacking in Iterator

--- src/subbitpackedarrayiterator.h
+++ src/subbitpackedarrayiterator.h
@@ -6,15 +6,30 @@
 template <uint16_t num_states, std::size_t num_values>
 struct kt::SubBitPackedArray<num_states, num_values>::Iterator
 {
-  using iterator_category = std::bidirectional_iterator_tag;
+  using iterator_category = std::forward_iterator_tag;
   using value_type = uint16_t;
 
-  Iterator(SubBitPackedArray* ptr, uint32_t index) : ptr_{ptr}, index_{index} {}
+  Iterator(SubBitPackedArray* ptr, uint32_t value_index)
+      : ptr_{ptr},
+        entry_index_{value_index / kStatesPer4ByteWord},
+        chunk_value_index_{static_cast<uint8_t>(value_index % kStatesPer4ByteWord)}
+  {
+    if (entry_index_ >= ptr_->getEntrySize())
+    {
+      return;
+    }
+
+    chunk_ = ptr_->entries_[entry_index_].getData();
+
+    for (size_t i = 0; i < chunk_value_index_; ++i)
+    {
+      chunk_ /= num_states;
+    }
+  }
 
   value_type operator*() const
   {
-    return ptr_->entries_[index_ / kStatesPer4ByteWord].get(
-        kPowerLookupTable[index_ % kStatesPer4ByteWord], num_states);
+    return chunk_ % num_states;
   }
   value_type operator->() const
   {
@@ -22,39 +37,41 @@ struct kt::SubBitPackedArray<num_states, num_values>::Iterator
   }
   Iterator& operator++()
   {
-    index_++;
+    chunk_value_index_++;
+    if (chunk_value_index_ == kStatesPer4ByteWord)
+    {
+      entry_index_++;
+      chunk_value_index_ = 0;
+      chunk_ = ptr_->entries_[entry_index_].getData();
+    }
+    else
+    {
+      chunk_ /= num_states;
+    }
     return *this;
   }
   Iterator operator++(int)
   {
     Iterator tmp = *this;
-    ++index_;
-    return tmp;
-  }
-  Iterator& operator--()
-  {
-    index_--;
-    return *this;
-  }
-  Iterator operator--(int)
-  {
-    Iterator tmp = *this;
-    --index_;
+    ++(*this);
     return tmp;
   }
   friend bool operator==(const Iterator& a, const Iterator& b)
   {
-    return a.ptr_ == b.ptr_ && a.index_ == b.index_;
-  };
+    return a.ptr_ == b.ptr_ && a.entry_index_ == b.entry_index_ &&
+           a.chunk_value_index_ == b.chunk_value_index_;
+  }
 
   friend bool operator!=(const Iterator& a, const Iterator& b)
   {
-    return a.ptr_ != b.ptr_ || a.index_ != b.index_;
-  };
+    return !(a == b);
+  }
 
 private:
   SubBitPackedArray* ptr_;
-  uint32_t index_;
+  uint32_t entry_index_;
+  uint32_t chunk_;
+  uint8_t chunk_value_index_;
 };
 
 #endif  // _SUBBITPACKEDARRAY_ITERATOR_H_
